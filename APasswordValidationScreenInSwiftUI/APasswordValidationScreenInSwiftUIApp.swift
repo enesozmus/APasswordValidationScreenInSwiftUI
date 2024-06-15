9b73744ad9624249d71dc59ac7f92081764adf61
enesozmus <ensozmus@gmail.com>
2024-06-15 11:08:41 +0300

    Initial Commit

---
+++ APasswordValidationScreenInSwiftUI/APasswordValidationScreenInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APasswordValidationScreenInSwiftUIApp.swift
+//  APasswordValidationScreenInSwiftUI
+//
+//  Created by enesozmus on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct APasswordValidationScreenInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APasswordValidationScreenInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APasswordValidationScreenInSwiftUI
+//
+//  Created by enesozmus on 15.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
